df9f1948f3d98f81be4da3f3717240a6339c1e0b
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2022-12-09 22:57:50 +0530

    removed context

--- src/context/user/userContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext();
-UserContext.displayName = "UserContext"
-
-export default UserContext;
--- src/pages/UserInformation/Account/Account.jsx
+++ src/pages/UserInformation/Account/Account.jsx
@@ -1,16 +1,5 @@
-import { useContext, useEffect } from "react";
-import UserContext from "../../../context/user/userContext";
-
 function Account() {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  useEffect(() => {
-    setTimeout(() => {
-      setCurrentUser({
-        email: "hahahahah no email for u",
-      });
-    }, 3000);
-  }, [setCurrentUser]);
-  return <div>This is about {currentUser.email}</div>;
+  return null;
 }
 
 export default Account;
