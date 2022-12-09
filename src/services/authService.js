536d83b1a01e67dea362957f7dd46f7c3b6bb29b
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2022-12-10 01:07:42 +0530

    login expiry + invalid values validation in protected routes

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,6 +2,9 @@ import http from './httpService'
 import config from './../config.json'
 
 const sessionKey = "user";
+// const currentDate = new Date().toISOString();
+let date_ob = new Date();
+let currentDate = (("0" + date_ob.getDate()).slice(-2) + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + date_ob.getFullYear() + " " + date_ob.getHours() + ":" + date_ob.getMinutes() + ":" + date_ob.getSeconds());
 
 http.setJwt(getJwt())
 
@@ -22,9 +25,19 @@ export function logout() {
 }
 
 export function getCurrentUser() {
+    let user = localStorage.getItem(sessionKey);
     try {
-        const user = localStorage.getItem(sessionKey);
-        return user;
+        if (user && JSON.parse(user).jwtToken !== undefined) {
+            if (JSON.parse(user).expiresOn > currentDate) { // means date is not expired
+                return user;
+            } else {
+                localStorage.removeItem(sessionKey);
+                return null
+            }
+        } else {
+            localStorage.removeItem(sessionKey);
+            return null
+        }
     } catch (error) {
         return null
     }
