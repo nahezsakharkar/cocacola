dce499c7134301abe4c2e8fc6bd193da0b0e95c2
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2022-12-13 23:00:09 +0530

    integrated api in ShowGroup.jsx

--- src/services/scheduleService.js
+++ src/services/scheduleService.js
@@ -15,8 +15,22 @@ export async function createGroup(formValues) {
     return response.data
 }
 
+export const getGroupsByScheduleStatus = async (scheduledStatus) => {
+    const user = JSON.parse(localStorage.getItem(sessionKey));
+    const response = await http.get(baseURL + "api/groups/byScheduledStatus?scheduledstatus=" + scheduledStatus,
+        {
+            headers: {
+                Authorization: user.jwtToken
+            }
+        }
+    );
+    return response.data;
+}
+
+
 const schedule = {
-    createGroup
+    createGroup,
+    getGroupsByScheduleStatus
 }
 
 export default schedule
